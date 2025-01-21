9cdb0405205673cb1a625bba83536cd4fe21dc32
vishal <vishaltanwar275@gmail.com>
2025-01-21 17:37:24 +0530

    build

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,1 +1,4 @@
-export const BASE_URL='http://localhost:4000/api/v1'
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://tour-travel-backend-mzsn.onrender.com/api/v1"
+    : "http://localhost:4000/api/v1";
